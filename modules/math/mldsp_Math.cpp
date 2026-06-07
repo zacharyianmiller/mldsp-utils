e4c7b7375f83afd4c758e07443cbb10c708a7971
zacharyianmiller <zacharyianmiller1@gmail.com>
2026-06-07 15:33:36 -0500

    sol 2 is even

--- modules/math/mldsp_Math.cpp
+++ modules/math/mldsp_Math.cpp
@@ -4,3 +4,13 @@ const bool mldsp::Math::isOdd(const int value)
 {
     return value % 2 /* == 1 */;
 }
+
+const bool mldsp::Math::isEven(const int value)
+{
+    return value % 2 == 0;
+}
+
+const bool mldsp::Math::isMultipleOf(const int dividend, const int divisor)
+{
+    return dividend % divisor == 0;
+}
--- modules/math/mldsp_Math.hpp
+++ modules/math/mldsp_Math.hpp
@@ -6,6 +6,10 @@ namespace Math
 
 const bool isOdd(const int value);
 
+const bool isEven(const int value);
+
+const bool isMultipleOf(const int dividend, const int divisor);
+
 }; // namespace Math
 
 }; // namespace mldsp
